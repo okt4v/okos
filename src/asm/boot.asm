91aaba7df28f298b3ade1df9b9ec75571bed8cd6
Oktavian Mayer <oktavianmayer@icloud.com>
2026-01-28 15:48:57 +0100

    fixed crash loop on boot

--- src/asm/boot.asm
+++ src/asm/boot.asm
@@ -3,146 +3,117 @@ bits 32
 section .multiboot
 align 8
 multiboot_header_start:
-  dd 0xe85250d6
-  dd 0 
-  dd multiboot_header_end - multiboot_header_start 
-  dd -(0xe85250d6 + 0 + (multiboot_header_end - multiboot_header_start))
-
-  dw 0 
-  dw 0 
-  dd 8 
+    dd 0xe85250d6
+    dd 0
+    dd multiboot_header_end - multiboot_header_start
+    dd -(0xe85250d6 + 0 + (multiboot_header_end - multiboot_header_start))
+    
+    align 8
+    dw 0
+    dw 0
+    dd 8
 multiboot_header_end:
 
-section .bss 
+section .bss
 align 16
 stack_bottom:
-  resb 16384    ;16 KiB stack 
+    resb 16384
 stack_top:
+
 align 4096
 p4_table:
-  resb 4096
+    resb 4096
 p3_table:
-  resb 4096
+    resb 4096
 p2_table:
-  resb 4096
+    resb 4096
 
 section .text
-global _start 
-extern kernel_main 
+global _start
+extern kernel_main
 
 _start:
-  mov esp, stack_top 
-  push ebx
-  push eax
-
-  call setup_page_tables
-  call enable_paging
-  call setup_long_mode
-
-  lgdt [gdt64.pointer]
-
-  jmp gdt64.code:long_mode_start
+    mov esp, stack_top
+    mov edi, ebx
+    mov esi, eax
+    
+    call setup_page_tables
+    call enable_paging
+    
+    lgdt [gdt64.pointer]
+    
+    jmp 0x08:long_mode_start
 
 setup_page_tables:
-  mov eax, p3_table
-  or eax, 0b11
-  mov [p4_table], eax 
-
-  mov eax, p2_table
-  or eax, 0b11
-  mov [p3_table], eax 
-
-  mov ecx, 0 
+    mov eax, p3_table
+    or eax, 0b11
+    mov [p4_table], eax
+    
+    mov eax, p2_table
+    or eax, 0b11
+    mov [p3_table], eax
+    
+    mov ecx, 0
 .map_p2_table:
-  mov eax, 0x200000
-  mul ecx 
-  or eax, 0b10000011
-  mov [p2_table + ecx * 8], eax
-  inc ecx 
-  cmp ecx, 512
-  jne .map_p2_table
-
-  ret 
+    mov eax, 0x200000
+    mul ecx
+    or eax, 0b10000011
+    mov [p2_table + ecx * 8], eax
+    inc ecx
+    cmp ecx, 512
+    jne .map_p2_table
+    
+    ret
 
 enable_paging:
-  mov eax, p4_table
-  mov cr3, eax 
-  
-  mov eax, cr4
-  or eax, 1 << 5 
-  mov cr4, eax 
+    mov eax, p4_table
+    mov cr3, eax
+    
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+    
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+    
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+    
+    ret
 
-  mov ecx, 0xC0000080
-  rdmsr
-  or eax, 1 << 8 
-  wrmsr 
-
-  mov eax, cr0 
-  or eax, 1 << 31
-  mov cr0, eax 
-
-  ret 
-
-setup_long_mode:
-  ret 
-
-bits 64 
+bits 64
 long_mode_start:
-  mov ax, gdt64.data 
-  mov ss, ax 
-  mov ds, ax 
-  mov es, ax 
-  mov fs, ax 
-  mov gs, ax 
-
-  pop rdi 
-  pop rsi 
-
-  call kernel_main
-
-  cli
+    ; Set stack pointer
+    mov rsp, 0x200000
+    and rsp, -16
+    
+    ; Load segments (skip ss)
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    
+    ; Call kernel (rdi and rsi already set from edi/esi)
+    call kernel_main
+    
+    ; Hang if kernel returns
+    cli
 .hang:
-  hlt 
-  jmp .hang
+    hlt
+    jmp .hang
 
-section .rodata 
+section .rodata
+align 16
 gdt64:
-  dq 0 
-.code: equ $ - gdt64
-  dq (1<<43) | (1<<44) | (1<<47) | (1<<53)
-.data: equ $ - gdt64
-  dq (1<<44) | (1<<47)
+    dq 0
+.code: equ 0x08
+    dq (1<<43) | (1<<44) | (1<<47) | (1<<53)
+.data: equ 0x10
+    dq (1<<44) | (1<<47)
 .pointer:
-  dw $ - gdt64 - 1 
-  dq gdt64 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    dw 23
+    dq gdt64
--- src/c/kernel.c
+++ src/c/kernel.c
@@ -1,4 +1,14 @@
 void kernel_main(unsigned long magic, unsigned long addr) {
+  (void)magic;
+  (void)addr;
+
+  // Write "OK" to screen
+  unsigned char *vga = (unsigned char *)0xB8000;
+  vga[0] = 'O';
+  vga[1] = 0x0F;
+  vga[2] = 'K';
+  vga[3] = 0x0F;
+
   while (1) {
     __asm__("hlt");
   }
