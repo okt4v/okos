abcd0a6c74df1fe291d4d66e8cbca9402639e4d2
Oktavian Mayer <oktavianmayer@icloud.com>
2026-01-28 16:44:48 +0100

    starting on terminal funcs

--- src/c/drivers/terminal.c
+++ src/c/drivers/terminal.c
@@ -1,1 +1,20 @@
-void main() {}
+#include "../inc/terminal.h"
+
+static unsigned char *vga_buffer = (unsigned char *)0xB8000;
+static int cursor_x = 0;
+static int cursor_y = 0;
+static unsigned char color = 0x0F;
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
+void terminal_initialize(void) { terminal_clear(); }
+
+void terminal_clear(void) {
+  for (int i = 0; i < VGA_WIDTH * VGA_HEIGHT * 2; i += 2) {
+    vga_buffer[i] = ' ';
+    vga_buffer[i + 1] = color;
+  }
+  cursor_x = 0;
+  cursor_y = 0;
+}
--- src/c/kernel/kernel.c
+++ src/c/kernel/kernel.c
@@ -1,13 +1,10 @@
+#include "../inc/terminal.h"
+
 void kernel_main(unsigned long magic, unsigned long addr) {
   (void)magic;
   (void)addr;
 
-  // Write "OK" to screen
-  unsigned char *vga = (unsigned char *)0xB8000;
-  vga[0] = 'O';
-  vga[1] = 0x0F;
-  vga[2] = 'K';
-  vga[3] = 0x0F;
+  terminal_initialize();
 
   while (1) {
     __asm__("hlt");
